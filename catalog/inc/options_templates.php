a78823a2ba159ccf2236cdce86d6bdc951a40cea
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-08-27 14:46:08 +0100

    Add import/export options and theme activation

--- catalog/inc/options_templates.php
+++ catalog/inc/options_templates.php
@@ -1,3 +1,4 @@
+</form><!--close off previous form-->
 <style>
   .CodeMirror {
     height: 200px;
@@ -7,8 +8,61 @@
     overflow-x: auto;
     height: 200px;
   }
+  .import {
+    font: bold 13px Helvetica, Arial, sans-serif;
+    text-decoration: none !important;
+    padding: 5px;
+    text-shadow: 0px 1px 0px rgba(255, 255, 255, 0.5);
+    transition: all 0.218s ease 0s;
+    color: #333 !important;
+    background: #fff;
+    border: 1px solid #ACACAC;
+    border-radius: 2px;
+    cursor: pointer;
+    box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.06);
+  }
 </style>
 <?php
+  // activate a theme
+  if (isset($_GET['activate'])) {
+    $file = GSDATAOTHERPATH . $this->id . '/templates.xml';
+    $template = GSDATAOTHERPATH . $this->id . '/templates/' . $_GET['activate'] . '.xml';
+
+    if (file_exists($file)) {
+      $xml = new SimpleXMLExtended($file, 0, true);
+
+      $xml->current = null;
+      $xml->current->addCData($_GET['activate']);
+    } else {
+      // theme/template does not exist
+    }
+  }
+
+  // importing a template
+  if (!empty($_FILES)) {
+    $upload = $_FILES['importFile'];
+
+    if ($upload['type'] == 'text/xml') {
+      $filename = $upload['name'];
+      $themename = basename($filename, '.xml');
+      $destination = GSDATAOTHERPATH . $this->id . '/templates/' . $filename;
+
+      if (!file_exists($destination)) {
+        move_uploaded_file($upload['tmp_name'], GSDATAOTHERPATH . $this->id . '/templates/' . $filename);
+
+        $msg = str_replace('%s', '<strong>' . $themename . '</strong>', i18n_r($this->id . '/OPTIONS_UPLOAD_THEME_SUCC'));
+        $isSuccess = true;
+      } else {
+        $msg = i18n_r($this->id . '/OPTIONS_UPLOAD_THEME_FAIL_NAME');
+        $isSuccess = false;
+      }
+    } else {
+      $msg = i18n_r($this->id . '/OPTIONS_UPLOAD_THEME_FAIL_TYPE');
+      $isSuccess = false;
+    }
+  }
+
+  // exporting a template for download
   if (isset($_GET['export'])) {
     $template = $_GET['export'] . '.xml';
     $templatesDir = GSDATAOTHERPATH . $this->id . '/templates/' . $template;
@@ -18,7 +72,8 @@
 
     $exportFile = str_replace(GSROOTPATH, $GLOBALS['SITEURL'], $tmpDir);
   }
-  // saved
+
+  // saving options
   if (isset($_POST['submitted'])) {
     $fields = array('main', 'header', 'category', 'product', 'featured', 'i18nsearch-product', 'footer');
     $xml = new SimpleXMLExtended('<templates/>');
@@ -46,7 +101,6 @@
   $themes    = $templates->getThemes();
   $current   = $templates->getCurrentTheme();
   $templates = $templates->getTemplates();
-
 ?>
 <!--select theme/template-->
 <?php if (isset($_GET['export']) && isset($template)) : ?>
@@ -56,29 +110,36 @@
   });
   </script>
 <?php endif; ?>
-<input type="hidden" name="current" value="<?php echo $current; ?>"/>
-<p>
-  <select id="theme_select" class="text" style="width:250px;" name="template">
-    <?php foreach ($themes as $theme) : ?>
-    <option <?php if ($theme == $current) echo 'selected="selected"'; ?>value="<?php echo $theme; ?>"><?php echo $theme; ?></option>
-    <?php endforeach; ?>
-  </select>&nbsp;&nbsp;&nbsp;
-  <input class="submit" name="submitted" value="<?php i18n('ACTIVATE_THEME'); ?>" type="submit">
-
-  <input class="submit import" name="import" value="<?php i18n($this->id . '/IMPORT'); ?>" type="submit">
-  <input class="submit export" name="export" value="<?php i18n($this->id . '/EXPORT'); ?>" type="submit">
-</p>
-
+<form action="" method="post" enctype="multipart/form-data">
+  <p>
+    <select id="theme_select" class="text" style="width:200px;" name="template">
+      <?php foreach ($themes as $theme) : ?>
+      <option <?php if ($theme == $current) echo 'selected="selected"'; ?>value="<?php echo $theme; ?>"><?php echo $theme; ?></option>
+      <?php endforeach; ?>
+    </select>&nbsp;&nbsp;&nbsp;
+    <input class="submit activate" name="submitted" value="<?php i18n('ACTIVATE_THEME'); ?>" type="submit">
+  </p>
+  <p>
+    <input class="import" name="importFile" value="<?php i18n($this->id . '/IMPORT'); ?>" type="file">
+    <input class="submit" name="import" value="<?php i18n($this->id . '/IMPORT'); ?>" type="submit">
+    <input class="submit export" name="export" value="<?php i18n($this->id . '/EXPORT'); ?>" type="submit">
+  </p>
+</form>
 
 <script>
 $(document).ready(function() {
+  $('.activate').click(function() {
+    window.location = '<?php echo $adminUrl; ?>&options=templates&activate=' + $('#theme_select').val();
+    return false;
+  });
   $('.export').click(function() {
     window.location = '<?php echo $adminUrl; ?>&options=templates&export=' + $('#theme_select').val();
     return false;
   });
 });
 </script>
-
+<form action="" method="post">
+<input type="hidden" name="current" value="<?php echo $current; ?>"/>
 <!-- main catalog page template -->
 <p>
   <h4 style="font-weight: bold;"><?php i18n($this->id . '/MAIN'); ?> : </h4>
