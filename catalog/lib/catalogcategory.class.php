ff9174bf5e6bc7f1e19bdcfd3d867cf18d7c48f7
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-08-27 16:21:08 +0100

    Add getField method to category class

--- catalog/lib/catalogcategory.class.php
+++ catalog/lib/catalogcategory.class.php
@@ -19,7 +19,27 @@ class CatalogCategory {
       $this->data['id'] = basename($file, '.xml');
     }
   }
-  
+
+  public function getField($field) {
+    $field = strtolower($field);
+
+    if ($field == 'photos') {
+      // format photos field
+      if (isset($this->data[$field]->photo)) {
+        return (array) $this->data[$field]->photo;
+      } else {
+        return array();
+      }
+    } elseif ($field == 'url') {
+      return $this->getUrl();
+    } elseif (isset($this->data[$field])) {
+      // normal field
+      return (string) $this->data[$field];
+    } else {
+      return 'boo';
+    }
+  }
+
   public function __call($name, $args) {
     if (substr($name, 0, 3) == 'get') {
       $field = strtolower(substr($name, 3));
